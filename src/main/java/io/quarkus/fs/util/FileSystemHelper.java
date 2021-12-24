43ad347c992c926be405a2fb74f98ade06978c40
Martin Panzer <postremus1996@googlemail.com>
2021-12-24 19:56:30 +0100

    Run CI against more JDK vendors

--- src/main/java/io/quarkus/fs/util/FileSystemHelper.java
+++ src/main/java/io/quarkus/fs/util/FileSystemHelper.java
@@ -1,11 +1,18 @@
 package io.quarkus.fs.util;
 
+import io.quarkus.fs.util.sysfs.ConfigurableFileSystemProviderWrapper;
+import io.quarkus.fs.util.sysfs.FileSystemWrapper;
+import io.quarkus.fs.util.sysfs.PathWrapper;
+
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.AccessMode;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.spi.FileSystemProvider;
 import java.util.Map;
+import java.util.Set;
 
 public class FileSystemHelper {
     /**
@@ -43,7 +50,9 @@ public class FileSystemHelper {
      * @return
      */
     public static Path ignoreFileWriteability(Path path) {
-        // Do nothing. Some vendors' openjdk11 check that the paths filesystem equals the system on inside ZipFileSystemProvider.newFileSystem(Path, env), causing an UnsupportedOperationException.
-        return path;
+        FileSystemProvider provider = new ConfigurableFileSystemProviderWrapper(path.getFileSystem().provider(),
+                Set.of(AccessMode.WRITE));
+        FileSystem fileSystem = new FileSystemWrapper(path.getFileSystem(), provider);
+        return new PathWrapper(path, fileSystem);
     }
 }
